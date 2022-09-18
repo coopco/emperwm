de002bfa3be4dca36987dca870211874ad253340
Connor Cooper <cooper.connor.17@gmail.com>
2022-09-19 01:18:36 +1000

    Initial commit

---
+++ xephyr.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+CUR_DIR="$(dirname $(readlink -f $0))"
+SCREEN_SIZE=${SCREEN_SIZE:-800x600}
+XDISPLAY=${XDISPLAY:-:1}
+EXAMPLE=${EXAMPLE:-local_test}
+# APP=${APP:-st}
+APP=${APP:-alacritty}
+
+Xephyr +extension RANDR -screen ${SCREEN_SIZE} ${XDISPLAY} -ac &
+XEPHYR_PID=$!
+
+sleep 1
+env DISPLAY=${XDISPLAY} "$CUR_DIR/target/debug/Eihwaz" &
+WM_PID=$!
+
+trap "kill $XEPHYR_PID && kill $WM_PID" SIGINT SIGTERM exit
+
+env DISPLAY=${XDISPLAY} ${APP} &
+wait $WM_PID
+kill $XEPHYR_PID
